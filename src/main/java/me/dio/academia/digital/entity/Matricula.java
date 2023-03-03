b3bdafe14b2d9218f17f387e2ae533132549e2c6
Renê Alves <renepvh11@gmail.com>
2023-03-03 15:47:25 -0400

    implementando a class Matricula

--- src/main/java/me/dio/academia/digital/entity/Matricula.java
+++ src/main/java/me/dio/academia/digital/entity/Matricula.java
@@ -1,4 +1,9 @@
 package me.dio.academia.digital.entity;
 
+import java.time.LocalDateTime;
+
 public class Matricula {
+    private Long id;
+    private Aluno aluno;
+    private LocalDateTime dataDaMatricula = LocalDateTime.now();
 }
