1a82f0368357b88f44843b67c4b472bbef663a47
Renê Alves <renepvh11@gmail.com>
2023-03-03 17:29:53 -0400

    implementando as anotacoes das classes Aluno, AvaliacaoFisica, Matricula

--- src/main/java/me/dio/academia/digital/entity/Matricula.java
+++ src/main/java/me/dio/academia/digital/entity/Matricula.java
@@ -1,9 +1,23 @@
 package me.dio.academia.digital.entity;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_matriculas")
 public class Matricula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
     private LocalDateTime dataDaMatricula = LocalDateTime.now();
 }
