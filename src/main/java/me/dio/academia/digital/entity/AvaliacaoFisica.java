244eff4e13b680de35acd020b1122b9b4722d9b3
Renê Alves <renepvh11@gmail.com>
2023-03-07 10:37:40 -0400

    tirando import nao usado

--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -1,6 +1,5 @@
 package me.dio.academia.digital.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
